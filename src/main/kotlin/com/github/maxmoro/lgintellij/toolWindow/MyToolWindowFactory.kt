619766de09ac35ce4c8345537f3e1e1810f15a6a
GitHub Action <action@github.com>
2025-10-14 00:43:07 +0000

    Template cleanup

--- src/main/kotlin/com/github/maxmoro/lgintellij/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/maxmoro/lgintellij/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.maxmoro.lgintellij.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.maxmoro.lgintellij.MyBundle
+import com.github.maxmoro.lgintellij.services.MyProjectService
 import javax.swing.JButton
 
 
